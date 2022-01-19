30227243d9b988cefd29cc6722f5eb6c4c20eba5
Beka0530 <94052420+Beka0530@users.noreply.github.com>
2022-01-19 19:58:53 +0600

    crypto

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from './components/header';
 import SectionFour from './components/section-four/section-four';
 import SectionTwo from './components/section-two/section-two';
 import Implementation from './components/implementation';
+import SectionTen from "./components/section-ten/section-ten";
+import SectionEleven from "./components/section-eleven/section-eleven";
+import SectionSeven from "./components/section-seven/section-seven";
 
 function App() {
     return (
@@ -13,6 +16,9 @@ function App() {
             <Benefits />
             <SectionFour />
             <Implementation />
+            <SectionSeven />
+            <SectionTen />
+            <SectionEleven />
         </>
     );
 }
